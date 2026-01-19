d32afb6896effecf5ede8020d4de83e29396db44
Aaroosh <aaroosh.2012@gmail.com>
2026-01-19 23:23:47 +0530

    adds functionality to split TextNodes

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 
 class TestUtils(unittest.TestCase):
     def test_text(self):
@@ -8,6 +8,30 @@ class TestUtils(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_spliting_text_nodes(self):
+        node = TextNode("This is text with a `code block` word", TextType.PlainText)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.Code)
+
+        expected_result = [
+                            TextNode("This is text with a ", TextType.PlainText),
+                            TextNode("code block", TextType.Code),
+                            TextNode(" word", TextType.PlainText),
+                          ]
+
+        self.assertEqual(new_nodes, expected_result)
+
+        node2 = TextNode("This is text with a **bolded phrase** in the middle", TextType.PlainText)
+
+        new_nodes2 = split_nodes_delimiter([node2], "**", TextType.BoldText)
+
+        expected_result2 = [
+                                TextNode("This is text with a ", TextType.PlainText),
+                                TextNode("bolded phrase", TextType.BoldText),
+                                TextNode(" in the middle", TextType.PlainText),
+                            ]
+
+        self.assertEqual(new_nodes2, expected_result2)
 
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
+import re
 
 
 def text_node_to_html_node(text_node: TextNode):
@@ -24,3 +25,43 @@ def text_node_to_html_node(text_node: TextNode):
             return HTMLNode(tag = "img", props= props)
         case _:
             raise Exception("Cannot find TextType specified")
+
+
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType):
+    new_nodes = list()
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.PlainText:
+            new_nodes.append(old_node)
+            continue
+        
+        textToSplit = old_node.text
+        if textToSplit.count(delimiter) % 2 == 1:
+            raise Exception("Markdown Syntax error: Delimiter not present in pairs")
+
+        #Find all occurences of delimiter
+        matches = list(re.finditer(delimiter, textToSplit))
+
+        index = 0
+        startIndex = 0
+        isPlainText = True
+
+        while startIndex < len(textToSplit) and index < len(matches):
+            curMatch = matches[index]
+            textSliced = textToSplit[startIndex: curMatch.start()]
+
+            if isPlainText:
+                new_nodes.append(TextNode(textSliced, TextType.PlainText))
+            else:
+                new_nodes.append(TextNode(textSliced, text_type))
+
+            index = index + 1
+            startIndex = curMatch.end()
+            isPlainText = not isPlainText
+        
+        # covers up a case where a plain text block is present in the end
+        if (startIndex < len(textToSplit)):
+            print(textToSplit[startIndex: len(textToSplit)])
+            new_nodes.append(TextNode(textToSplit[startIndex: len(textToSplit)], TextType.PlainText))
+
+    return new_nodes
